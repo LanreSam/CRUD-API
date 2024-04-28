35b68bcbc7c040aa47940114f26eaae3688a5c1f
lanresam <ezechelanre@gmail.com>
2024-04-28 16:07:15 +0100

    created delete endpoint

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
+let users = [
     // {
     //     firstName:"John",
     //     lastName:"Doe",
@@ -28,5 +28,20 @@ router.post('/', (req, res) => {
 
     res.send(`User with the name ${user.firstName} added to th mock DB`)
 });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id)
+
+    res.send(`User with the id ${id} deleted from the mock DB`)
+})
 
 export default router;
