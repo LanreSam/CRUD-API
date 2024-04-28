9f0dc695df2283b5ee3f465e42436c2d13e356f2
lanresam <ezechelanre@gmail.com>
2024-04-28 15:43:55 +0100

    Fixed post route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,32 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
-const router = express.Router()
+
+const router = express.Router();
+
+const users = [
+    // {
+    //     firstName:"John",
+    //     lastName:"Doe",
+    //     age:50,
+    //     eyeColor:"blue"
+    // },
+    // {
+    //     firstName:"Jane",
+    //     lastName:"Doe",
+    //     age:40,
+    //     eyeColor:"green"
+    // }
+]
 
 // all users routes starts from here
-router.get('/', (req, res) => res.send('Hello Users'));
+router.get('/', (req, res) => res.send(users));
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id:uuidv4() });
+
+    res.send(`User with the name ${user.firstName} added to th mock DB`)
+});
 
 export default router;
